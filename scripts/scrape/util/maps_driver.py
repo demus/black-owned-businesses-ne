73bc89c9535535594b7e9ecdabdbaa6c2e87f444
crumpstrr33 <jscott12009@gmail.com>
2020-06-18 19:27:57 -0700

    Remove unnecessary imports

--- scripts/scrape/util/maps_driver.py
+++ scripts/scrape/util/maps_driver.py
@@ -6,8 +6,6 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
